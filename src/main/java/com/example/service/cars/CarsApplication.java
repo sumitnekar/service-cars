6b8b8be532e95bfd895c93228ebd18a7758a75b6
Sumit Nekar <sumit.nekar@gmail.com>
2020-12-28 20:47:03 +0530

    Testing worklow

--- src/main/java/com/example/service/cars/CarsApplication.java
+++ src/main/java/com/example/service/cars/CarsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CarsApplication.class, args);
 	}
